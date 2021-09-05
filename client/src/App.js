c644be87c29dda6c23e41ec21f3b103d3c96c4e3
sayantan1821 <ksayantan1823@gmail.com>
2021-09-06 02:51:46 +0530

    github icon added

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,9 @@ import Todo from "./Todo";
 import login from "./auth/login";
 import register from "./auth/register";
 import Alert from "./layout/Alert";
-import { Switch, Route, BrowserRouter } from "react-router-dom";
+import { Switch, Route, BrowserRouter, Link, Redirect } from "react-router-dom";
+import { VscGithub } from "react-icons/vsc";
+
 
 import { Provider } from "react-redux";
 import store from "./store";
@@ -30,6 +32,7 @@ const App = () => {
           <Route exact path="/register" component={register} />
           <Route exact path="/todo" component={Todo} />
         </Switch>
+        <a style={{position: 'absolute', bottom: '0', left: '1%'}} href='https://github.com/sayantan1821/To-Do-List-React'><VscGithub size='40'/></a>
       </BrowserRouter>
     </Provider>
   );
