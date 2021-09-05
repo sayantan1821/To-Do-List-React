eec3aa3cd59a6dba38bbd053a6f2e19cb260fe14
sayantan1821 <ksayantan1823@gmail.com>
2021-09-05 23:27:25 +0530

    reverse chat items order

--- client/src/Todo.js
+++ client/src/Todo.js
@@ -11,8 +11,8 @@ class Todo extends Tasks {
         const { tasks } = this.state;
         return (
             <div className="App flex">
-                <Paper elevation={3} className="container">
-                    <div className="heading">TO-DO</div>
+                <Paper elevation={3} className="container" style={{width: '50vw'}}>
+                    <div className="heading">Sayantan's TO-DO List</div>
                     <form
                         onSubmit={this.handleSubmit}
                         className="flex"
@@ -37,7 +37,9 @@ class Todo extends Tasks {
                         </Button>
                     </form>
                     <div>
-                        {tasks.map((task) => (
+                        {tasks.slice(0).reverse().map((task) => {
+                            let date =  new Date(task.date)
+                            return (
                             <Paper
                                 key={task._id}
                                 className="flex task_container"
@@ -57,7 +59,7 @@ class Todo extends Tasks {
                                     {task.task}
                                     
                                 </div>
-                                <p>[{task.date.toDateString()}]</p>
+                                <p style={{color: 'GrayText'}}>[ {date.toDateString()} ]</p>
                                 <Button
                                     onClick={() => this.handleDelete(task._id)}
                                     color="secondary"
@@ -65,7 +67,7 @@ class Todo extends Tasks {
                                     delete
                                 </Button>
                             </Paper>
-                        ))}
+                        )})}
                     </div>
                 </Paper>
             </div>
